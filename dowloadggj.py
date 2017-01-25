fe0b39e1c12da1fce9ba20112be9a1fda5b731f5
Emma Westman <eewestman@gmail.com>
2017-01-25 12:56:55 +0100

    added function for retreving problem ids

--- dowloadggj.py
+++ dowloadggj.py
@@ -17,10 +17,37 @@ PROBLEM_B = '5634697451274240'
 PROBLEM_C = '5738606668808192'
 PROBLEM_D = '5636311922769920'
 
+
+def retrive_problem_ids(page):
+    #print page
+    remaining_page = page
+    ids = []
+    while "\"id\":" in remaining_page:
+	index = remaining_page.find("\"id\":")
+	index += len("\"id\":")
+	remaining_page = remaining_page[index:]
+	# finding the start qutation mark for problem id
+	start_index = remaining_page.find("\"")
+	# find the end qutation marl for problem id
+	end_index = remaining_page[start_index+1:].find("\"")
+	
+  	print start_index
+	print end_index
+	# extract problem id string
+	problem_id = remaining_page[start_index:end_index+3]
+	ids.append(problem_id)
+	
+	remining_page = remaining_page[end_index+3:]
+	#print remaining_page[start_index-10:end_index+10]
+	print problem_id
+	
+    return ids
+
 answer = urllib2.urlopen(baseurl)
-s = answer.read()
-html = etree.HTML(s)
-print html
+#print answer.read()
+list_of_ids = retrive_problem_ids(answer.read())
+print list_of_ids
+
 
 '''def retrieve_sol(problem,io_set_id,username):
     requesturl = 'https://code.google.com/codejam/contest/6254486/scoreboard/do/?cmd=GetSourceCode&problem='+
