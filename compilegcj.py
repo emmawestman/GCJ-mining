12a5278e8984310baedb025048638b72c762ca32
Alexandra Back <alexanra@03.ed5352.exj.cse.rh66.ii.2.hb.s.cdal.chalmers.se>
2017-01-31 12:49:32 +0100

    script for compiling javafiles

--- compilegcj.py
+++ compilegcj.py
@@ -7,10 +7,16 @@ PATH = os.path.join('..','solutions_qualification_2016','java')
 def compile_java():
 	for root, dirs, files in os.walk(PATH):
 		for f in files:
-						
-			#proc = subprocess.Popen(cmd, shell=True, stdout=subproces.PIPE, stderr=subprocess.PIPE)
-			#out, err = proc.communicate()
- 			#print(out)
- 			#print(err) 
+			subprocess.check_call(['javac', os.path.join(root,f) ])
 
+
+
+def remove_class_files():
+	for root, dirs, files in os.walk(PATH):
+		filelist = [ f for f in files if (f.endswith(".class") or f.endswith(".txt") or f.endswith(".py~")) ]
+		for f in filelist:			
+			os.remove(os.path.join(root,f))
+		
+
+remove_class_files()
 compile_java()
