fe0aac7419ef1854bbaead7aea038d616198b32c
Emma Westman <eewestman@gmail.com>
2017-02-01 16:37:16 +0100

    started on compile for C# if no main

--- compilegcj.py
+++ compilegcj.py
@@ -56,8 +56,13 @@ def compile_python(path):
 			filename = filename + '.in'
 			
 			cmd = ['python ' + os.path.join(root,f) + ' < ' + os.path.join(PATH_INPUT,filename)]
-			p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+			p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 			output, errors = p.communicate()
+			if len(errors) > 0:
+				print 'error'
+			else:
+				print 'Successfully compiled!'
+
 	
 			
 		
@@ -65,6 +70,51 @@ def compile_python(path):
 				print error
 			else:
 				print 'Successfully copiled and ran ' + os.path.join(root,f)'''
+
+def compile_csharp(path):
+	for root, dirs, files in os.walk(path):
+		for f in files:
+			regexp = "/C#/"
+			index = root.find(regexp)
+			filename = root[index+len(regexp):]
+			index = filename.find('/')
+			user = filename[index+1:]
+			filename = filename[:index]
+			print 'Compiling problem: ' + filename + ', for user: ' + user
+			filename = filename + '.in'
+			
+			cmd = ['mcs ' + os.path.join(root,f) + ' < ' + os.path.join(PATH_INPUT,filename)]
+			p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			output, errors = p.communicate()
+			if len(errors) > 0:
+				# fix issue where no main is missing
+				if "does not contain a static `Main' method suitable for an entry point" in errors:
+					# find function in file to call from main
+					# find namespace
+
+					# create main file and call some function...
+					csharp_main('SolveProblem', f, 'GoogleCodeJam.Pancakes', root)
+					# run main file instead
+					cmd = ['mcs ' + os.path.join(root,'TestMain.cs ') + os.path.join(root, f) + ' < ' + os.path.join(PATH_INPUT,filename)]
+					p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+					output, errors = p.communicate()
+					if len(errors) > 0 :
+						print 'I give up'
+						print errors
+					else: 
+						print 'Successfully added and ran main!' 
+			else:
+				print 'Successfully compiled!'
+
+def csharp_main(function_name, filename, namespace, path):
+	index = len(filename) -3
+	filename = filename[:index]
+	main_file= os.path.join(path, 'TestMain.cs')         
+	file1 = open(main_file, "w")
+	file_content = 'namespace ' + namespace + '\n' + '{ \n class TestMain \n { \n static void Main() \n { \n' + filename + '.' + function_name + '();' + ' \n } \n } \n }'
+	file1.write(file_content)
+	file1.close()
+
 
 
 
@@ -93,7 +143,8 @@ def compile_language(language):
 	elif language == "C++":
 		print "C++ has no compile script yet"
 	elif language == 'C#':
-		print "C# has no compile script yet"
+		csharp_path = build_language_path('C#')
+		compile_csharp(csharp_path)
 	elif language == "Python":
 		python_path = build_language_path('Python')
 		compile_python(python_path)
@@ -101,12 +152,15 @@ def compile_language(language):
 		print language ++ " is not one of the selected languages, try: java, C, C++, C# or Python"
 
 
-compile_language("Python")
+#compile_language("Python")
+compile_language("C#")
+
+
 
 #remove_class_files()
 #compile_language('java')
-folder_name = build_language_path('java')
-run_java_files(folder_name)
+#folder_name = build_language_path('java')
+#run_java_files(folder_name)
 
 
 #remove_class_files()
