bcce6ebbce47b1e3133a530f9a89a9637cc7eca4
Alexandra Back <alexandra1.back@gmail.com>
2017-02-10 15:05:48 +0100

    minor changes

--- compile_python.py
+++ compile_python.py
@@ -27,56 +27,59 @@ def compile_python(path):
 	return succes_nbr, nbr_of_files
 
 def run_python_2x(file_path,path_input):
-	errors = run_commando('python ',file_path,path_input)
+	errors = run_python_commando('python ',file_path,path_input)
 	if len(errors) > 0:
-		error_name = filter_information('\w+Error',None,errors)[0]
-			if error_name =='ModuleNotFound':
-				missing_module_name = filter_information('\'\w+\'',None,errors)[0]
-				missing_module_name = missing_module_name.replace('\'','')
-				if missing_module_name == 'devtools':
-					rename_stuff_in_file('','devtools',file_path)
+		print errors
+		error_name = filter_information('\w+Error',None,errors)
+		print error_name
+		if error_name =='ModuleNotFound':
+			missing_module_name = filter_information('\'\w+\'',None,errors)[0]
+			missing_module_name = missing_module_name.replace('\'','')
+			if missing_module_name == 'devtools':
+				rename_stuff_in_file('','devtools',file_path)
+				run_python_2x(file_path,path_input)
+			elif missing_module_name == 'run':
+				rename_stuff_in_file('runpy','run',file_path)
+				run_python_2x(file_path,path_input)
+			else :
+				pip_errors = pip_install_module(missing_module_name)
+				if len(pip_errors>0):
+					print pip_errors
+				else:
 					run_python_2x(file_path,path_input)
-				elif missing_module_name == 'run':
-					rename_stuff_in_file('runpy','run',file_path)
-					run_python_2x(file_path,path_input)
-				else :
-					pip_errors = pip_install_module(missing_module_name)
-					if len(pip_errors>0):
-						print pip_errors
-					else:
-						run_python_2x(file_path,path_input)
-			elif error_name = 'SyntaxError':
-					return run_python_3x(file_path,path_input)
-			else:
-				print errors
+		elif error_name == 'SyntaxError':
+			return run_python_3x(file_path,path_input)
+		else:
+			print errors
+			return 0
 	else :
 		return 1
 
 def run_python_3x(file_path,path_input):
-	errors = run_commando('python ',file_path,path_input)
+	errors = run_python_commando('python3',file_path,path_input)
 	if len(errors) > 0:
 		error_name = filter_information('\w+Error',None,errors)[0]
-			if error_name =='ModuleNotFound':
-				missing_module_name = filter_information('\'\w+\'',None,errors)[0]
-				missing_module_name = missing_module_name.replace('\'','')
-				if missing_module_name == 'devtools':
-					rename_stuff_in_file('','devtools',file_path)
+		if error_name =='ModuleNotFound':
+			missing_module_name = filter_information('\'\w+\'',None,errors)[0]
+			missing_module_name = missing_module_name.replace('\'','')
+			if missing_module_name == 'devtools':
+				rename_stuff_in_file('','devtools',file_path)
+				run_python_3x(file_path,path_input)
+			elif missing_module_name == 'run':
+				rename_stuff_in_file('runpy','run',file_path)
+				run_python_3x(file_path,path_input)
+			else :
+				pip_errors = pip_install_module(missing_module_name)
+				if len(pip_errors>0):
+					print pip_errors
+				else:
 					run_python_3x(file_path,path_input)
-				elif missing_module_name == 'run':
-					rename_stuff_in_file('runpy','run',file_path)
-					run_python_3x(file_path,path_input)
-				else :
-					pip_errors = pip_install_module(missing_module_name)
-					if len(pip_errors>0):
-						print pip_errors
-					else:
-						run_python_3x(file_path,path_input)
-			else:
-				print errors
+		else:
+			print errors
+			return 0
 	else :
 		return 1
  
-
 def rename_stuff_in_file(new_module_name,old_module_name,file_path):
 	#read old content
 	file_manager = open(file_path,'r')
@@ -89,14 +92,14 @@ def rename_stuff_in_file(new_module_name,old_module_name,file_path):
 	file_handle.write(file_contents)
 	file_handle.close()
 
-def pip_install_module(pip_version,module_name)
+def pip_install_module(pip_version,module_name):
 	cmd = [pip_version + ' install ' + module_name]
 	p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	output, errors = p.communicate()
 	return errors
 
 def run_python_commando(pythonversion,path_file,path_input):	
-	cmd = [pythonversion + path_file + ' < ' + ]
+	cmd = [pythonversion + path_file + ' < ' + path_input]
 	p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	output, errors = p.communicate()
 	return errors
