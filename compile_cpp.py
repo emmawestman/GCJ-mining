9166e29dc165fdcc7416e7ec8f1d940d14835a2e
Emma Westman <eewestman@gamil.com>
2017-02-10 15:52:43 +0100

    changed print messages

--- compile_cpp.py
+++ compile_cpp.py
@@ -3,30 +3,15 @@ import subprocess
 
 PATH_INPUT = os.path.realpath(os.path.join('..','input_qualification_2016'))
 
-'''
-def compile_cpp2(path):
-	nbr_of_files = 0
-	succes_nbr = 0
-	for root, dirs, files in os.walk(path):
-		for f in files:
-			nbr_of_files += 1
-			if (f.endswith(".cpp") or f.endswith(".cc") or f.endswith(".CPP") or f.endswith(".c++") or f.endswith(".cp") or f.endswith(".cxx")):
-				try:
-					subprocess.check_call(['g++', os.path.join(root,f), '-o', os.path.join(root,f) ]) # remove ending in f at last argumet
-				except subprocess.CallProcessError:
-					pass
-				succes_nbr += 1
-	return succes_nbr, nbr_of_files'''
-
+	
 def compile_cpp(path):
 	#number of files that successfylly compiles
 	succes_nbr = 0
 	nbr_of_files = 0
-	print path
 	for root, dirs, files in os.walk(path):
 		for f in files:
 			nbr_of_files += 1
-			print 'file nbr: ' + str(nbr_of_files)
+			print 'compiling file nbr: ' + str(nbr_of_files)
 			regexp = "/C++/"
 			index = root.find(regexp)
 			filename = root[index+len(regexp):]
@@ -57,11 +42,11 @@ def run_cpp(path):
 	succes_nbr = 0
 	nbr_of_files = 0
 	for root, dirs, files in os.walk(path):	
-		# only try to run the executable files
+		# only try to run the executable 
 		filelist = [f for f in files if '.' not in f]
 		for f in filelist:
-			print f
 			nbr_of_files += 1
+			print 'running file nbr: ' + str(nbr_of_files)
 			regexp = "/C++/"
 			index = root.find(regexp)
 			filename = root[index+len(regexp):]
@@ -79,7 +64,6 @@ def run_cpp(path):
 				print errors
 			else:
 				succes_nbr += 1
-				#print 'Successfully compiled!'
 	return succes_nbr, nbr_of_files
 
 
