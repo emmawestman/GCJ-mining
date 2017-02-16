a3a749f1ed44369dbcdc69789c82bbce1604caad
alexandraback <alexandra1.back@gmail.com>
2017-02-16 13:51:37 +0100

    refactoring

--- compile_python.py
+++ compile_python.py
@@ -18,18 +18,15 @@ def compile_python(c_id):
 			if f.endswith('.py'):
 				nbr_of_files += 1
 				print nbr_of_files
-				regexp = "/Python/"
-				index = root.find(regexp)
-				filename = root[index+len(regexp):]
-				index = filename.find('/')
-				user = filename[index+1:]
-				filename = filename[:index]
-				filename = filename + '.in'
-				print filename
+				filename = get_input_file(root)
 				path_file = os.path.join(root,f)
+				print "File name " + filename
 				path_input = os.path.join(PATH_INPUT,filename)
 				succes_nbr += run_python_2x(path_file,path_input)
 	return succes_nbr, nbr_of_files
+
+def get_input_file(root):
+	return re.search('\d+\_\d', root).group(0)
 
 def run_python_2x(file_path,path_input):
 	errors = run_python_commando('timeout 120s python ',file_path,path_input)
@@ -38,26 +35,30 @@ def run_python_2x(file_path,path_input):
 		if len(error_list)>0:
 			error_name = error_list[0]			
 			if error_name =='ImportError':
-				missing_module_name = filter_information('named\s\w+',None,errors)[0]
-				missing_module_name = missing_module_name.replace('named','')
-				if missing_module_name == 'devtools':
-					rename_stuff_in_file('','import devtools',file_path)
-					run_python_2x(file_path,path_input)
-				elif missing_module_name == 'run':
-					rename_stuff_in_file('runpy','run',file_path)
-					run_python_2x(file_path,path_input)
-				else :
-					pip_errors = pip_install_module('pip',missing_module_name)
-					if len(pip_errors)>0:
-						print pip_errors
-						return 0
-					else:
-						return run_python_2x(file_path,path_input)
+				handle_import_error(file_path,path_input,errors)
 			elif error_name == 'SyntaxError':
 				return run_python_3x(file_path,path_input)
 		print errors
 		return 0
 	return 1
+
+def handle_import_error(file_path,path_input,errors,pip_version):
+	missing_module_name = filter_information('named\s\w+',None,errors)[0]
+	missing_module_name = missing_module_name.replace('named','')
+	if missing_module_name == 'devtools':
+		rename_stuff_in_file('','import devtools',file_path)
+		run_python_2x(file_path,path_input)
+	elif missing_module_name == 'run':
+		rename_stuff_in_file('runpy','run',file_path)
+		run_python_2x(file_path,path_input)
+	else :
+		pip_errors = pip_install_module(pip_version,missing_module_name)
+		if len(pip_errors)>0:
+			print pip_errors
+			return 0
+		else:
+			return run_python_2x(file_path,path_input)
+
 
 def run_python_3x(file_path,path_input):
 	errors = run_python_commando('timeout 120s python3 ',file_path,path_input)
@@ -66,21 +67,7 @@ def run_python_3x(file_path,path_input):
 		if len(error_list)>0:
 			error_name = error_list[0]			
 			if error_name =='ImportError':
-				missing_module_name = filter_information('named\s\w+',None,errors)[0]
-				missing_module_name = missing_module_name.replace('named','')
-				if missing_module_name == 'devtools':
-					rename_stuff_in_file('','import devtools',file_path)
-					run_python_3x(file_path,path_input)
-				elif missing_module_name == 'run':
-					rename_stuff_in_file('runpy','run',file_path)
-					run_python_3x(file_path,path_input)
-				else :
-					pip_errors = pip_install_module('pip3',missing_module_name)
-					if len(pip_errors)>0:
-						print pip_errors
-						return 0
-					else:
-						return run_python_3x(file_path,path_input)
+				handle_import_error(file_path,path_input,errors,pip_version)
 		else:
 			print errors
 			return 0
