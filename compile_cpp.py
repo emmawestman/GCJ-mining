068127de556e8251fe69300b60949d89c7e24112
Emma Westman <eewestman@gamil.com>
2017-02-16 16:39:07 +0100

    added check for exit code, timeout commands, for c and c++2

--- compile_cpp.py
+++ compile_cpp.py
@@ -19,20 +19,26 @@ def compile_cpp(c_id):
 			print 'compiling c++ file nbr: ' + str(nbr_of_files)
 			
 			user, filename = get_compile_info('C++', root, f)
-			cmd = ['timeout 120s g++ ' + os.path.join(root,f) + ' -o ' + os.path.join(root,filename)]
+			#cmd = ['timeout 120s g++ ' + os.path.join(root,f) + ' -o ' + os.path.join(root,filename)]
+			cmd = ['timeout 120s g++ -std=c++0x ' + os.path.join(root,f) + ' -o ' + filename]
   			errors = copile_one_cpp_file(cmd)
-			if len(errors) > 0:
-				#try with c++ 11 instead
-				cmd = ['timeout 120s g++ -std=c++0x ' + os.path.join(root,f) + ' -o ' + filename]
-  				errors = copile_one_cpp_file(cmd)
-	
+			exit_code = check_exit_code()
+			if exit_code == 0:
 				if len(errors) > 0:
-					print 'failed to run problem with g++11: ' + filename + ', for user: ' + user
+					'''
+					#try with c++ 11 instead
+					cmd = ['timeout 120s g++ -std=c++0x ' + os.path.join(root,f) + ' -o ' + filename]
+  					errors = copile_one_cpp_file(cmd)
+	
+					if len(errors) > 0:
+						print 'failed to run problem with g++11: ' + filename + ', for user: ' + user
+						print errors
+					else:
+						succes_nbr +=1
+					'''
 					print errors
 				else:
-					succes_nbr +=1
-			else:
-				succes_nbr += 1
+					succes_nbr += 1
 	return succes_nbr, nbr_of_files
 
 
@@ -58,12 +64,14 @@ def run_cpp(c_id):
 		
 			cmd = ['timeout 120s ' + os.path.join(root,f) + ' < ' + os.path.join(PATH_INPUT,input_file)]
   			errors = run_one_cpp_file(cmd)
-			if len(errors) > 0:
-				print 'Error Running problem: ' + root
-				#print errors
+			exit_code = check_exit_code()
+			if exit_code == 0:
+				if len(errors) > 0:
+					print 'Error Running problem: ' + root
+					#print errors
 
-			else:
-				succes_nbr += 1
+				else:
+					succes_nbr += 1
 	return succes_nbr, nbr_of_files
 
 
