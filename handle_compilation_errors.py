0253cc66a17bdc8ab6a5bdf026ff9fbf3b2926ed
alexandraback <alexandra1.back@gmail.com>
2017-02-22 15:32:55 +0100

    helper module

---
+++ handle_compilation_errors.py
@@ -0,0 +1,24 @@
+import re
+
+def find_old_new_regex(regex_list,file_contents):
+	for item in regex_list:
+		found_matches = find_out_what_regex(item,file_contents)
+		if found_matches is not None:
+			return found_matches
+	return None
+
+def find_out_what_regex(regex,file_contents):
+	found_matches = re.search(regex,file_contents)
+	if found_matches is not None:
+		match = found_matches.group(0)
+		return match
+	return None	
+
+def rename_inorout_file(regex_list,input_file,file_contents):
+	found_match = find_old_new_regex(regex_list,file_contents)
+	if found_match is not None:
+			copyofmatch = found_match
+			find_what_to_replace = re.search(r'\((.*?)\)',found_match).group(1)
+			new_input = found_match.replace(find_what_to_replace,input_file)
+			file_contents = file_contents.replace(copyofmatch,new_input)
+	return file_contents
