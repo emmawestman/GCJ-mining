7ca69ac53db164cbdd12256b11317fe3616dfebd
Alexandra Back <alexandra1.back@gmail.com>
2017-02-27 15:10:44 +0100

    refactoring

--- compile_csharp.py
+++ compile_csharp.py
@@ -6,7 +6,7 @@ from stuff_module import create_folder
 from compile_support_module import *
 from finding_regexes import *
 
-	
+
 def compile_run_csharp(c_id):
 	path = os.path.realpath(os.path.join('..','solutions_' + c_id, 'C#' ))
 	PATH_INPUT = os.path.realpath(os.path.join('..','input_' + c_id))
@@ -29,7 +29,7 @@ def remove_files_in_a_user_solution(root):
 	for file in filelist:			
 		os.remove(os.path.join(root,file))
 
-def compile_csharp(root,csharp_file_p,csharp_file_p_dependecy,input_file,flag):
+def compile_and_run_csharp(root,csharp_file_p,csharp_file_p_dependecy,input_file,flag):
 	csharp_file = ''
 	if flag is not None:
 		csharp_file = '-r:'+flag+'.dll '
@@ -38,32 +38,10 @@ def compile_csharp(root,csharp_file_p,csharp_file_p_dependecy,input_file,flag):
 		csharp_file = csharp_file + ' ' + os.path.join(root,csharp_file_p_dependecy)
 	errors = compile_csharp_command(csharp_file)
 	if len(errors)>0:
-		print errors
-		if "does not contain a static `Main' method suitable for an entry point" in errors:  #TODO make a method for errors
-			# find function in file to call from main
-			# find namespace
-			namespace = find_namespace(csharp_file_p, root)
-			# create main file and call some function...
-			filter_candidates = filter_candidate_functions(os.path.join(root,csharp_file_p))
-			csharp_main(filter_candidates[0], csharp_file_p, namespace, root,input_file)
-			# run main file instead
-			return compile_csharp (root,'TestMain.cs',csharp_file_p,input_file,flag)
-		if 'The type or namespace name' in errors:
-			old_regex = filter_information('\`\w+\'',None,errors)[0]
-			old_regex = old_regex.replace('`','')
-			old_regex = old_regex.replace ('\'','')
-			new_regex = filter_information('\`\w+[.]\w+\'',None,errors)
-			if len(new_regex)>0 :
-				new_regex = new_regex[0]
-				new_regex = new_regex.replace('`','')
-				new_regex = new_regex.replace ('\'','')
-				return compile_csharp (root,csharp_file_p,csharp_file_p_dependecy,input_file,new_regex)
-		print errors
+		handle_compile_errors(errors)
 		return 0
 	else :
-		csharp_= csharp_file_p.split('.')[0]
-		csharp_exe = csharp_ + '.exe'
-		print 'CSHARP FILE ' + csharp_exe
+		csharp_= csharp_file_p.replace('.cs','.exe')
 		if csharp_file_p_dependecy is not None:
 			csharp_file_p = csharp_file_p_dependecy
 		return run_csharp(input_file,root,os.path.join(root,csharp_exe),csharp_file_p)
@@ -86,6 +64,24 @@ def run_csharp(input_file,root,csharp_exe,original_class_file):
 		return 0
 	return 1
 
+
+def handle_compile_errors(errors):
+	if "does not contain a static `Main' method suitable for an entry point" in errors:  #TODO make a method for errors
+		create_main_file()
+		return compile_csharp (root,'TestMain.cs',csharp_file_p,input_file,flag)
+		if 'The type or namespace name' in errors:
+			old_regex = filter_information('\`\w+\'',None,errors)[0]
+			old_regex = old_regex.replace('`','')
+			old_regex = old_regex.replace ('\'','')
+			new_regex = filter_information('\`\w+[.]\w+\'',None,errors)
+			if len(new_regex)>0 :
+				new_regex = new_regex[0]
+				new_regex = new_regex.replace('`','')
+				new_regex = new_regex.replace ('\'','')
+				return compile_csharp (root,csharp_file_p,csharp_file_p_dependecy,input_file,new_regex)
+		print errors
+
+
 def run_csharp_command(csharp_exe,input_file):
 	if input_file is not None :
 		csharp_exe = csharp_exe + ' < ' + input_file
@@ -95,44 +91,4 @@ def run_csharp_command(csharp_exe,input_file):
 	return errors
 
 
-def change_input_streams(input_file,csharp_file,root):
-	file_contents = get_contents_of_file(file_path)
-	file_contents = rename_input_file('new StreamReader\((.*)\)','\"'+input_file+'\"',input_file,file_contents)
-	new_output = '\"' + os.path.join(root,'output.txt') + '\"'
-	file_contents = rename_output_file('new StreamWriter\((.*)\)',new_output,file_contents,root)
-	write_new_contents_to_the_file(csharp_file,file_contents)
 
-def csharp_main(full_function_decl, filename, namespace, path,input_file):
-	index = len(filename) -3
-	filename = filename[:index]
-	function_name = re.findall(r'(\w+)',full_function_decl)[0]
-	main_file= os.path.join(path, 'TestMain.cs')         
-	file1 = open(main_file, "w")
-	file_content = 'namespace ' + namespace + '\n' + '{ \n class TestMain \n { \n static void Main() \n { \n' + filename + '.' + function_name + build_main_function(full_function_decl,input_file) + ' \n } \n } \n }'
-	file1.write(file_content)
-	file1.close()
-
-#ONLY CHOOSING STATIC FUNCTIONS
-def filter_candidate_functions(file_path):
-	file_contents = get_contents_of_file(file_path)
-	p = re.compile('static\s(?:int|void|long|string)\s(\w+\([\w+\s]*?\))')
-	return p.findall(file_contents)
-
-#ONLY HANDLING ONE StreamWriter and StreamReader
-def build_main_function(filtered_function,input_file):
-	main_string = '('
-	p = re.compile('(\w+\s\w+)')
-	list_of_args = p.findall(filtered_function) 
-	for x in range(0,len(list_of_args)) :
-		group = list_of_args[x]
-		arg_type = group.split(' ')[0]
-		arg_decl = ''
-		if arg_type == 'StreamReader':
-			arg_decl = 'new StreamReader(\''+ input_file + '\')'
-		if arg_type == 'StreamWriter':
-			arg_decl = 'new StreamWriter(output.txt)'
-		main_string+= arg_decl
-		if x != len(list_of_args)-1 and ',' in main_string:
-			main_string+= ','
-	main_string+=');'
-	return main_string
