9c66ed8ba4352d4cddde9c59890eab1b8319bc16
Emma Westman <eewestman@gamil.com>
2017-03-07 15:14:04 +0100

    changed to dictonary

--- statistics/write_to_csv.py
+++ statistics/write_to_csv.py
@@ -1,56 +1,73 @@
 import csv 
 import os
 
-
 def read_csv_file(filename) :
 	path = os.path.join('../..', 'GCJ-backup', filename)
 	with open(path, 'rb') as csvfile:
 	    reader = csv.reader(csvfile, delimiter=' ', quotechar=',')
-	    content = ""
+	    content = []
 	    for row in reader:
-	        content +=  ', '.join(row) +'\n'
-	    return content
+	        content.append(row)
+        attr_names = content[0][1:]
 
-def write_to_csv_file(filename, matrix) :
-	path = os.path.join('../..', 'GCJ-backup', filename)
+        dict = {}
+        for row in content[1:] :
+            user_id = row[0]
+            user_dict = {}
+            
+            for idx, a in enumerate(attr_names) :
+                user_dict[a] = row[idx +1]
+            dict[user_id] = user_dict
+        return dict
+
+def write_to_csv_file(filename, dict) :
+    path = os.path.join('../..', 'GCJ-backup', filename)
+    users = dict.keys()
+    attr_names = dict[users[0]].keys()
+    first_row = ['user_id'] + attr_names
+    matrix = []
+    matrix.append(first_row)
+    for u in users :
+        values = dict[u].values()
+        row = [u] + values
+        matrix.append(row)
+
 	with open(path, 'wb') as csvfile:
 	    writer = csv.writer(csvfile, delimiter=' ', quotechar=',')
 	    for row in matrix:
 	       writer.writerow(row)
-
+    
 
 def create_csv(c_id, p_id, users, lang) :
 	complete_name = os.path.join('../..', 'GCJ-backup', c_id + '_' + p_id + '.csv') 
 	with open(complete_name, 'wb') as csvfile :
 		writer = csv.writer(csvfile, delimiter=' ', quotechar=',', quoting=csv.QUOTE_MINIMAL)  
 		#write header
-		writer.writerow(['username', 'rank', 'lang'])
+		writer.writerow(['user_id', 'rank', 'lang'])
 		i = 1 
 		for u in users :
 			result = [u, i, lang]
 			writer.writerow(result)
 			i += 1
 
-def change_column(c_id, p_id, username, column, new_value) : 
-	filename = c_id + '_' + p_id + '.csv'
-	content = read_csv_file(filename)
-	lines = content.split('\n')
-	matrix = [l.split(',') for l in lines]
-	new_lines = []
-	## find row of user
-	for l in lines :
-		attributes = l.split(',')
-		if attributes[0] == username :	
-			#edit row
-			attributes[column] = new_value
-		new_lines.append(attributes)
-	write_to_csv_file(filename, new_lines)
+def change_column(c_id, p_id, users, column_name, new_values) : 
+    filename = c_id + '_' + p_id + '.csv'
+    dict = read_csv_file(filename)
+    for idx, u in enumerate(users) :
+        user_dict = dict[u]
+        user_dict[column_name] = new_values[idx]
+    write_to_csv_file(filename, dict)
 	
 	#writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 	#writer.writerow(result)
+
 	
-create_csv('c123', 'p123', ['user1', 'user2', 'user3', 'user4', 'user5'], 'C++')
-change_column('c123', 'p123', 'user4', 2, 'java')
+create_csv('c123', 'p123',['user1', 'user2', 'user3', 'user4', 'user5'] , 'C++')
+print read_csv_file('c123_p123.csv')
+change_column('c123', 'p123', ['user1', 'user2', 'user3', 'user4', 'user5'], 'lang', ['C++', 'C++', 'Python', 'java', 'C++'])
+print read_csv_file('c123_p123.csv')
+change_column('c123', 'p123', ['user1', 'user2', 'user3', 'user4', 'user5'], 'compiler', ['-', '-', '3.5', '-', '-'])
+print read_csv_file('c123_p123.csv')
 
 
 
