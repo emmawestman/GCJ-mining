911551f6877279c0253561a84f9350a5408f55bf
Emma Westman <eewestman@gmail.com>
2017-03-08 10:18:54 +0100

    fixed errors, test on vm

--- statistics/write_to_csv.py
+++ statistics/write_to_csv.py
@@ -1,5 +1,6 @@
 import csv 
 import os
+import sys
 
 # import own modules from diffrent directory
 gcj_path = os.path.join(os.getcwd(), '../')
@@ -41,26 +42,41 @@ def write_to_csv_file(filename, dict) :
 	    for row in matrix:
 	       writer.writerow(row)
 
+def get_user_ids(c_id) :
+    path = os.path.realpath(os.path.join(get_HOME_PATH(),'solutions_' + c_id))
+    dict = {}
+    for l in get_LANGUAGE() :            
+        path_lang = os.path.join(path, l)
+        prob_ids = os.listdir(path_lang)
+        for p_id in prob_ids :
+            users = os.listdir(os.path.join(path_lang, p_id))
+            old_users = dict [p_id]
+            dict [p_id] =  old_users.append(users)
 
+        print dict
 
 def create_csv(c_id) :
-        path = os.path.realpath(os.path.join('..','solutions_' + c_id))
+        path = os.path.realpath(os.path.join(get_HOME_PATH(),'solutions_' + c_id))
         user_ids = []
-	    for l in get_LANGUAGE() :
-	        path_lang = os.path.join(path, l, p_id)
+        for l in get_LANGUAGE() :
+            path_lang = os.path.join(path, l)
             prob_ids = os.listdir(path_lang)
-			for p_id in prob_ids :
-				users = os.listdir(os.path.join(path_lang, p_id))
+            for p_id in prob_ids :
+                users = os.listdir(os.path.join(path_lang, p_id))
+                print os.path.join(path_lang, p_id)
+                print users
                 user_ids.append(users)
 
+        print user_ids
+
         complete_name = os.path.join('../..', 'GCJ-backup', c_id + '_' + p_id + '.csv') 
-	    with open(complete_name, 'wb') as csvfile :
-		    writer = csv.writer(csvfile, delimiter=' ', quotechar=',', quoting=csv.QUOTE_MINIMAL)  
-		    #write header
-		    writer.writerow(['user_id'])
-		    for u in user_ids :
-		    	result = [u]
-			    writer.writerow(result)
+        with open(complete_name, 'wb') as csvfile :
+            writer = csv.writer(csvfile, delimiter=' ', quotechar=',', quoting=csv.QUOTE_MINIMAL)  
+            #write header
+            writer.writerow(['user_id'])
+            for u in user_ids :
+                result = [u]
+                writer.writerow(result)
 
 def change_column(c_id, p_id, users, column_name, new_values) : 
     filename = c_id + '_' + p_id + '.csv'
@@ -71,13 +87,15 @@ def change_column(c_id, p_id, users, column_name, new_values) :
     write_to_csv_file(filename, dict)
 	
 
-	
-create_csv('c123', 'p123',['user1', 'user2', 'user3', 'user4', 'user5'])
+get_user_ids('11254486')	
+#create_csv('11254486')
+'''
 print read_csv_file('c123_p123.csv')
 change_column('c123', 'p123', ['user1', 'user2', 'user3', 'user4', 'user5'], 'lang', ['C++', 'C++', 'Python', 'java', 'C++'])
 print read_csv_file('c123_p123.csv')
 change_column('c123', 'p123', ['user1', 'user2', 'user3', 'user4', 'user5'], 'compiler', ['-', '-', '3.5', '-', '-'])
 print read_csv_file('c123_p123.csv')
+'''
 
 
 
