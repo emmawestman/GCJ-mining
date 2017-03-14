7ba99dce79358ed33c0d405886b1727255930bc0
Emma Westman <eewestman@gamil.com>
2017-03-14 14:38:55 +0100

    refactored c++ compile2 2 git pull

--- compile/compilegcj.py
+++ compile/compilegcj.py
@@ -42,29 +42,23 @@ from write_to_csv import *
 					
 
 def compile_language(language, p_id, dict):
-	a = -1
-	b = -1
-	c = -1
-	d = -1
 	if language == 'java':
 		cwd = os.getcwd()
-		a, b, dict = compile_java(p_id, dict)
-		c, d = run_java_files(p_id)
+		dict = compile_java(p_id, dict)
+		dict = run_java_files(p_id)
 		os.chdir(cwd)
 	elif language == 'C':
-		a, b, dict = compile_c(p_id, dict)
-		c, d, dict = run_c(p_id, dict)
+		dict = compile_c(p_id, dict)
+		dict = run_c(p_id, dict)
 	elif language == "C++":
-		a, b, dict = compile_cpp(p_id, dict)
-		c, d, dict = run_cpp(p_id, dict)
+		dict = compile_cpp(p_id, dict)
+		dict = run_cpp(p_id, dict)
 	elif language == 'C#':
-		a, b, dict = compile_run_csharp(p_id, dict)
+		dict = compile_run_csharp(p_id, dict)
 	elif language == "Python":
-		a,b, dict = compile_python(p_id, dict)
+		dict = compile_python(p_id, dict)
 	else: 	
 		print language + " is not one of the selected languages, try: java, C, C++, C# or Python"
-	print language + ': ' + str(a) + ' out of ' + str(b) + ' programs compiled sucessfully'
-	print language + ': ' + str(c) + ' out of ' + str(d) + ' programs ran sucessfully'
 	return dict
 
 # compile all languages
