bb59d80e6e9afff94fffd7261467889b701a4907
alexandraback <alexandra1.back@gmail.com>
2017-03-14 13:15:51 +0100

    refactoring

--- compile/compile_java/compile_java.py
+++ compile/compile_java/compile_java.py
@@ -40,9 +40,7 @@ def run_java_file(user_path,problem_folder,user_folder,class_name, p_id):
 
 def run_java_command(class_name,args):	
 	cmd = ['timeout 30s java ' + class_name + ' ' + args ]
-	p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-	output, errors = p.communicate()
-	exit_code = p.returncode
+	exit_code, errors = full_exe_cmd(cmd)
 	if exit_code == 0:
 		if errors.find('Exception in thread')!= -1:
 			return 0,errors
@@ -50,7 +48,7 @@ def run_java_command(class_name,args):
 	else:
 		return 0, None
 
-
+def getUser
 	
 
 def compile_java(p_id, dict):
@@ -62,14 +60,8 @@ def compile_java(p_id, dict):
 			nbr_of_files += 1
 			if (f.endswith(".java")):
 				full_path = os.path.join(root,f)
-				cmd = ['timeout 30s javac ' + full_path]
-				p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-				output, errors = p.communicate()
-				exit_code = p.returncode
-				# update user dict
-				user_id = get_user_id(full_path)
-				user_dict = dict[user_id]
-				user_dict['compiler_version'] = '-'
+				exit_code, errors = run_process_compile(full_path)
+				dict = set_compiler_to_none(dict,full_path)
 				if int(exit_code) == 0:
 					if 'Note' in errors or len(errors) == 0:
 						succes_nbr += 1
@@ -83,27 +75,40 @@ def compile_java(p_id, dict):
 	print dict
 	return succes_nbr, nbr_of_files, dict
 
+def run_process_compile(full_path) :
+	cmd = ['timeout 30s javac ' + full_path]
+	return run_process(cmd)
+
+def set_compiler_to_none(dict,full_path) :
+	user_id = get_user_id(full_path)
+	user_dict = dict[user_id]
+	user_dict['compiler_version'] = '-'
+	return dict
+
+
 def run_java_files(p_id) :
 	path = os.path.realpath(os.path.join(get_HOME_PATH(),'solutions_' + p_id, 'java'))
 	nbr_of_files = 0
 	succes_nbr = 0
+	#List problemfolders
 	problemfolders = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
+	
 	for problem_folder in problemfolders:
 		problemPATH = os.path.join(path, problem_folder)
+		#List userfolders
 		userfolders = [f for f in os.listdir(problemPATH) if os.path.isdir(os.path.join(problemPATH, f))]
 		for user_folder in userfolders:
 			nbr_of_files += 1
 			userPATH = os.path.join(problemPATH,user_folder)
-
-			#print userPATH
 			os.chdir(userPATH)
+			#Filter class name
 			java_file = [f for f in os.listdir(userPATH) if f.endswith('.java')][0] #TODO: ASSUMES THAT ONLY EXIST ONE JAVA FILE
 			class_file =[ f for f in os.listdir(userPATH) if (f.endswith(".class") and f.split('.')[0])==java_file.split('.')[0] ] #TODO : FULT MEN WHAT TO DO
+			
 			if len(class_file)>0:
 					class_name = class_file[0].split('.')[0]
 					succes_nbr += run_java_file(userPATH,problem_folder,user_folder,class_name, p_id)
 	return succes_nbr, nbr_of_files
-
 
 
 def get_exception_name(errors):
