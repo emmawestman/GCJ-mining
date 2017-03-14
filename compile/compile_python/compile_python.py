7f88ba812f7fed7769727d9daa9abaa52bdd2d51
Emma Westman <eewestman@gamil.com>
2017-03-14 15:45:42 +0100

    added mesurments to python code

--- compile/compile_python/compile_python.py
+++ compile/compile_python/compile_python.py
@@ -15,60 +15,52 @@ def compile_python(p_id, dict):
 	path = os.path.realpath(os.path.join(get_HOME_PATH(),'solutions_' + p_id, 'Python' ))
 	print path
 	input_path = PATH_INPUT + p_id
-	succes_nbr = 0
-	nbr_of_files = 0
+
 	for root, dirs, files in os.walk(path):
 		for f in files:
 			if f.endswith('.py'):
-				nbr_of_files += 1
+                print 'Runing python file: ' + root
 				filename = get_input_file(root)+'.in'
 				path_file = os.path.join(root,f)
 				path_input = os.path.join(input_path,filename)
-				succes_of_file, dict = run_python_2x(path_file,path_input,p_id,root, dict)
-				# update dict compiled YES/NO
-				user_id = get_user_id(path_file)
-				user_dict = dict[user_id]
-				if succes_of_file == 1 :
-					user_dict['compiled'] = 'YES'
-				else :
-					user_dict['compiled'] = 'NO'
-				succes_nbr += succes_of_file
-	return succes_nbr, nbr_of_files, dict
+				exit_code, errors, dict = run_python_2x(path_file,path_input,p_id,root, dict)
+				# update dict compiled 
+                dict = set_compile_exitcode(dict,full_path,exit_code)
+                dict = do_run_mesurments(exit_code, errors, dict, root)
+				
+	return dict
 
 
-def run_python(file_path,path_input,pythonversion):
+def run_python(file_path,path_input,pythonversion,dict):
 	print "Running file python3 " + file_path
 	args = ' < ' + path_input
 	return run_python_command(pythonversion,file_path, args, dict)
 
 
 def run_python_2x(file_path,path_input,p_id,root, dict):
-	errors, dict = run_python(file_path,path_input,'python')
-	if len(errors) > 0:
+	exit_code, errors, dict = run_python(file_path,path_input,'python')
+	if not int(exit_code) == 0 or not int(exit_code) == 124:
 		return handle_python_2x_errors(file_path,path_input,p_id,root,errors,dict) 			
-	return 1, dict
+	return exit_code, errors, dict
 
 
 def run_python_3x(file_path,path_input,p_id,root,dict):
-	errors, dict = run_python(file_path,path_input,'python3')
+	exit_code, errors, dict = run_python(file_path,path_input,'python3')
 	if len(errors) > 0:
 		return handle_python_3x_errors(errors,file_path,path_input,p_id,root,dict)
-	return 1, dict
+	return exit_code, errors, dict
 
 def run_python_command(pythonversion,path_file,args,dict):	
-	# update dictonry so verison is stored in csv
-	user_id = get_user_id(path_file)
-	user_dict = dict[user_id]
 	if pythonversion == 'python3 ' :
 		version = "3.5"
 	else:
 		version = "2.7"
-	user_dict['compiler_version'] = version
+    # update dictonry so verison is stored in csv
+	dict = set_compiler_version(dict,full_path,version)
 
-	cmd = [pythonversion + path_file + args]
-	p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	output, errors = p.communicate()
-	return errors, dict
+	cmd = pythonversion + path_file + args
+    exit_code, errors = full_exe_cmd(cmd)
+	return exit_code, errors, dict
 
 def handle_python_2x_errors(file_path,path_input,p_id,root,errors,dict):
 	error_name = get_error_name(errors)
@@ -84,11 +76,10 @@ def handle_python_2x_errors(file_path,path_input,p_id,root,errors,dict):
 		return run_python_2x(file_path,path_input,p_id,root,dict)
 	elif error_name == 'IndexError':
 		args = ' ' + path_input +' '+os.path.join(root,'output.txt')
-		errors,dict = run_python_command('python ',file_path,args,dict)
+		exit_code, errors, dict = run_python_command('python ',file_path,args,dict)
 		if len(errors) == 0:
-			return 1, dict
+			return exit_code, errors, dict
 	print errors
-	return 0, dict
 
 def handle_python_3x_errors(errors,file_path,path_input,p_id,root,dict):
 	error_name = get_error_name(errors)
@@ -101,5 +92,5 @@ def handle_python_3x_errors(errors,file_path,path_input,p_id,root,dict):
 		handle_python_file_not_found(path_input,root,p_id,file_path)
 		run_python_3x(file_path,path_input,p_id,root,dict)
 	print errors
-	return 0, dict
+
 
