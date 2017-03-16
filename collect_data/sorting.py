869fe80c3233e2614219f51d9a8c321334d5669d
Emma Westman <eewestman@gmail.com>
2017-03-16 22:00:17 +0100

    removed dupliactes in p_ids.in

--- collect_data/sorting.py
+++ collect_data/sorting.py
@@ -12,24 +12,6 @@ from stuff_module import *
 
 
 def select_folder(filename, path):
-<<<<<<< HEAD
-    language = ""
-    if filename == '.java':
-        language = 'java'
-    elif filename == '.c':
-        language = 'C'
-    elif filename == ".cpp" or filename == ".C" or filename == ".cc" or filename == ".CPP" or filename == ".c++" or filename == ".cp" or filename == ".cxx":
-        language = 'C++'
-    elif filename == '.cs':
-        language = 'C#'
-    elif filename == '.py':
-        language = 'Python'
-    else:
-        print "This is not one of the examined languages, this is: " + filename
-        # TODO remove this zip-file?
-        return -1
-    return os.path.join(path, language)
-=======
 	language = ""
 	if filename == '.java':
 		language = 'java'
@@ -46,7 +28,7 @@ def select_folder(filename, path):
 		# TODO remove this zip-file?
 		return -1,-1
 	return os.path.join(path, language),language
->>>>>>> csv2
+
 
 def get_file_ending(filename):
     index = filename.find(".")
@@ -78,51 +60,6 @@ def find_dest(file_names_list,PATH) :
 
 
 def sort_files(p_id):
-<<<<<<< HEAD
-    PATH = os.path.join(get_HOME_PATH(),'solutions_' + p_id)
-    # make a list of all files in the directory
-    all_zip_names = os.listdir(PATH)
-
-    # create new folders for languages
-    java_path = os.path.join(PATH, 'java')
-    create_folder(java_path)
-
-    c_path = os.path.join(PATH, 'C')
-    create_folder(c_path)
-
-    cpp_path = os.path.join(PATH, 'C++')
-    create_folder(cpp_path)
-
-    cs_path = os.path.join(PATH, 'C#')
-    create_folder(cs_path)
-
-    python_path = os.path.join(PATH, 'Python')
-    create_folder(python_path)
-
-    all_zip_names = get_zip_files(all_zip_names,PATH)
-    print all_zip_names
-    for username in all_zip_names:
-        file_names_list = zipfile.ZipFile(os.path.join(PATH, username)).namelist()
-        # find corerct language
-        dest = find_dest(file_names_list,PATH)
-        if not dest == -1 :
-            # create folder
-            dest = os.path.join(dest, username)
-            downloadgcj.create_folder(dest)
-
-            for filename in file_names_list:
-                print 'Sorting ' + p_id + ' for user: ' + username + ' into ' + dest
-                # unpack files into folder
-                zipfile.ZipFile(os.path.join(PATH, username)).extract(filename,dest)
-
-        #clean up, remove zip-file
-        os.remove(os.path.join(PATH, username))
-    print "Done sorting all zip files!"
-
-sort_files('5658068650033152_1')
-
-
-=======
 	PATH = os.path.join(get_HOME_PATH(),'datacollection','solutions_' + p_id)
 	dict = {}
 	# make a list of all files in the directory
@@ -159,10 +96,10 @@ sort_files('5658068650033152_1')
 			dest,language = select_folder(file_ending, PATH)
 			''' check that the language is valid'''
 			if dest != -1:
-				# username folder 
+				# username folder
 				dest = os.path.join(dest, username)
 				dict[username]={}
-				user_dict =dict[username] 				
+				user_dict =dict[username]
 				user_dict['language']= language
 				downloadgcj.create_folder(dest)
 				#print dest
@@ -172,4 +109,3 @@ sort_files('5658068650033152_1')
 		os.remove(os.path.join(PATH, username))
 	print "Done sorting all zip files!"
 	return dict
->>>>>>> csv2
