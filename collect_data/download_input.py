188ebff5dd6bf98dfa11484a27f79b9b688457a5
Emma Westman <eewestman@gmail.com>
2017-03-17 10:14:31 +0100

    updates for cloc

--- collect_data/download_input.py
+++ collect_data/download_input.py
@@ -13,18 +13,18 @@ from constants import *
 
 
 #retrives the input file in constest c_id, for prob A/B/C/D, of size small/large
-def retrive_input(c_id, prob, size, prob_id,token): 
+def retrive_input(c_id, prob, size, prob_id,token):
 	if size == "-small.practice.in":
 		s = '0'
 	else:
 		s = '1'
 	url = get_BASE() + c_id + '/dashboard/do/' + prob + size +'?cmd=GetInputFile&problem=' + prob_id + '&input_id=' + s + '&filename=' + prob + size+'&redownload_last=1&agent=website&csrfmiddlewaretoken=' + token
 	answer = urllib2.urlopen(url).read()
-	path = os.path.join(get_HOME_PATH(),'input')
+	path = os.path.join(get_HOME_PATH(),'datacollection','input')
 	create_folder(path)
 	with open(os.path.join(path,prob_id + "_" + s + '.in'), 'w') as f:
 		f.write(answer)
-     
+
 def download_all_input(c_id, prob, size, prob_ids,token):
 	print ['Problems ids: '] + prob_ids
 	for x in range(0,len(prob_ids)):
@@ -32,4 +32,3 @@ def download_all_input(c_id, prob, size, prob_ids,token):
 			p = prob[x]
 			prob_id = prob_ids[x]
 			retrive_input(c_id, p, s, prob_id,token)
-
--- statistics/cloc.py
+++ statistics/cloc.py
@@ -1,18 +1,20 @@
 import os
 import subprocess
-from write_to_csv import *
+import sys
+
 
 # import own modules from diffrent directory
-gcj_path = os.getcwd()
+gcj_path = os.path.join(os.getcwd(), '../')
 sys.path.insert(0, gcj_path)
 from constants import *
+from write_to_csv import *
 
 LANGUAGE = get_LANGUAGE()
 
 
 # creates on row with prob_id, langage, user, blank, comment, code
 def cloc_file(prob_id, lang, user) :
-	path = os.path.realpath(os.path.join(get_HOME_PATH(),'solutions_' + prob_id, lang, user))
+	path = os.path.realpath(os.path.join(get_HOME_PATH(),'datacollection', 'solutions_' + prob_id, lang, user))
 	print path
 	cmd = ['cloc ' + path ]
 	p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -29,9 +31,8 @@ def cloc_file(prob_id, lang, user) :
 # creates a file containing rows with the format describen in cloc_problem
 def cloc_problem(p_id) :
 	print 'CLOC for ' + str(p_id)
-	path = os.path.realpath(os.path.join(get_HOME_PATH(),'solutions_' + str(p_id)))
-	prob_ids = []
 	dict = read_csv_file(str(p_id) + '.csv')
+	#print dict
 	users = dict.keys()
 	for user in users :
 		user_dict = dict[user]
@@ -40,7 +41,7 @@ def cloc_problem(p_id) :
 		# update user dict
 		user_dict = dict[user]
 		user_dict['cloc'] = results[2]
-		user_dict['blanks'] = results[0]	
+		user_dict['blanks'] = results[0]
 		user_dict['comments'] = results[1]
 	# write to dict to file
 	write_to_csv_file(str(p_id) + '.csv', dict)
@@ -56,3 +57,4 @@ def cloc_all() :
 	for problem_id in get_PROBLEM_IDS(one_up):
 		cloc_problem(problem_id)
 
+cloc_all()
