7aab3a2cb014a8503163e89ceedb29f845734e26
Emma Westman <eewestman@gamil.com>
2017-03-27 15:02:52 +0200

    new approach

--- compile/compile_support_module.py
+++ compile/compile_support_module.py
@@ -102,7 +102,7 @@ def timeout( p ):
 # to compile
 def run_process(cmd):
     cmd = [cmd]
-    p = subprocess.Popen(cmd,shell=False,stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=os.setsid)
+    p = subprocess.Popen(cmd,shell=False,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
    
     timer = Timer(10, timeout, [p])
     try:
