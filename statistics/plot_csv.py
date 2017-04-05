d43676f79824a58f79e4e78888821b23b6da95ba
alexandraback <alexandra1.back@gmail.com>
2017-04-05 14:20:15 +0200

    plotbox

--- statistics/plot_csv.py
+++ statistics/plot_csv.py
@@ -16,6 +16,7 @@ from write_to_csv import *
 def create_one_frame() :
     dict_cid_to_pid = read_csv_file_to_dict('cid_pid_map_new.csv')
     #create frame for contest csv
+    list_of_frames = []
     for contest_id in get_CONTEST_IDS():
         print "CONTEST ID " + contest_id
         contest_dict = pandas.read_csv(os.path.realpath(os.path.join(get_HOME_PATH(),'GCJ-backup',contest_id+'.csv')))
@@ -45,7 +46,6 @@ def create_one_frame() :
                     problem_df = problem_df [['user_id','language']]
                     df = pandas.merge(df,problem_df, on = 'user_id',how = 'left')
 
-        mirror_df = df.copy(deep=True)
         #filter contestats that have not used the same language:
         list_of_same_lang = []
         list_of_diff_lang = []
@@ -61,16 +61,20 @@ def create_one_frame() :
 
         df = df.drop(df.index[list_of_same_lang])
         df1 = df.ix[:,1:3]
-        box = df1.boxplot(by='language_x')
-        plt.ylabel("rank")
-        plt.xlabel("language")
-        plt.title("Contest boxplot " + contest_id )
-        plt.suptitle("")
 
-        fig = box.get_figure()
-        fig_path = os.path.join(get_HOME_PATH(),'GCJ-backup','Figures',contest_id + '_rank_language_plot.png')
+        list_of_frames.append(df1.groupby(['language_x'], as_index=False).mean())
 
-        fig.savefig(fig_path)
+    box = pandas.concat(list_of_frames).boxplot(by='language_x')
+        #box = df1.boxplot(by='language_x')
+    plt.ylabel("rank")
+    plt.xlabel("language")
+    plt.title("Boxplot Rank vs Language For All Competitions" )
+    plt.suptitle("")
+
+    fig = box.get_figure()
+    fig_path = os.path.join(get_HOME_PATH(),'GCJ-backup','boxplot_rank_lang'+ '_rank_language_plot.png')
+
+    fig.savefig(fig_path)
 
 def plot_rank_language():
     dict_cid_to_pid = read_csv_file_to_dict('cid_pid_map_new.csv')
