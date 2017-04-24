898ba9069c51182baa2aecfbd467c9ce8a334528
alexandraback <alexandra1.back@gmail.com>
2017-04-24 21:26:30 +0200

    new java fix

--- compile/compile_java/compile_java.py
+++ compile/compile_java/compile_java.py
@@ -56,6 +56,10 @@ def compile_java(p_id, dict):
                 full_path = os.path.join(root,f)
                 b, exit_code, errors = compile_java_command(full_path)
                 # write compile statistics
+                if 'is public, should be declared in a file named' in errors :
+                    rename_class_file(full_path,errors)
+                    b,exit_code,errors = compile_java_command(full_path)
+                user_dict = dict[get_user_id(root)]
                 user_dict = dict[get_user_id(root)]
                 #default mesurments
                 set_compiler_version(user_dict,'-')
@@ -63,7 +67,7 @@ def compile_java(p_id, dict):
                 print 'COPILE EXIT CODE ' + str(exit_code)
                 print errors
                 set_compile_mesurments(user_dict, b, exit_code)
-                
+
 
 def handle_java_run_errors(errors,exit_code,root,class_name,input_path, user_dict):
     #print errors
--- compile/compile_java/handle_java_run_errors.py
+++ compile/compile_java/handle_java_run_errors.py
@@ -1,10 +1,17 @@
 import os
 import sys
+import re
 
 compile_path = os.path.join(os.getcwd(), '../')
 sys.path.insert(0, compile_path)
 from handle_compilation_errors import *
 
+def rename_class_file(file_path,errors):
+    real_name_regex = '(\w+\.java)\n'
+    real_name = re.findall(real_name_regex,errors)[0]
+    new_file_path = file_path.split('/')[:-1]
+    new_file_path = '/'+ reduce(os.path.join,new_file_path)+'/'+real_name
+    os.rename(file_path,new_file_path)
 
 def remove_missing_package(file_path):
     rename_stuff_in_file ('package\s.*','',file_path,1)
