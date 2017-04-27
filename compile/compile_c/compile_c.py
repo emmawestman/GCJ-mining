9ab9665b56f651f877dda90797c7a0a32755faf3
alexandraback <alexandra1.back@gmail.com>
2017-04-27 13:13:45 +0200

    changes

--- compile/compile_c/compile_c.py
+++ compile/compile_c/compile_c.py
@@ -14,6 +14,41 @@ sys.path.insert(0, gcj_path)
 from constants import *
 
 
+def compile_c_command(exe_path, file_path, user_dict):
+    cmd = 'gcc -o ' + exe_path  + ' ' + os.path.join(user_path,f)
+    exit_code, errors = run_process(cmd)
+
+    if 'undefined reference ' in errors :
+        set_compiler_version(user_dict, '-lm')
+        exit_code,errors = run_process (cmd + ' -lm')
+
+    if 'only allowed in ' in errors :
+        cmd = 'gcc -std=c11 -o' + exe_path  + ' ' + os.path.join(user_path,f)
+        set_compiler_version(user_dict, 'std=c11')
+        exit_code, errors = run_process(cmd)
+
+    return exit_code,errors
+
+def execute_c_command(exe_path, f, user_path, input_path, user_dict):
+    cmd = './' + exe_path + ' < ' + input_path
+    exit_code, errors = full_exe_cmd(cmd)
+    if 'iostream fatal error' in errors :
+        compile_with_gplus_cmd = 'g++ -std=c++0x ' + os.path.join(user_path,f) + ' -o ' + exe_path
+        run_process(cmd)
+        exit_code,errors = full_exe_cmd (os.path.join(user_path,f) + ' < ' + input_path)
+    measure_exe(exe_path,user_dict)
+    return exit_code, errors
+
+ def measure_exe(exe_path,user_dict):
+     try:
+         b = str(get_size_of_exe(exe_path))
+     except Exception, e:
+         b = '-'
+     else:
+         pass
+     finally:
+         set_exe_size(user_dict, b)
+
 
 def compile_c(p_id, dict):
     path = os.path.realpath(os.path.join(get_HOME_PATH(), 'datacollection', 'solutions_' + p_id, 'C' ))
@@ -24,29 +59,22 @@ def compile_c(p_id, dict):
         for f in os.listdir(user_path) :
             if has_valid_file_ending('C', f) :
                 print 'compiling c file for: ' + user + ' in problem ' + p_id
-                
+
                 # do compilation
                 exe_file = f.split('.')[0]
                 exe_path = os.path.join(user_path,exe_file)
-                cmd = 'gcc -o ' + exe_path  + ' ' + os.path.join(user_path,f)
-                exit_code, errors = run_process(cmd)  
-                try:
-                    b = str(get_size_of_exe(exe_path))
-                except Exception, e:
-                    b = '-'
-                else:
-                    pass
-                finally:  
-                    set_exe_size(user_dict, b)
-
-                # update dictonary
-                set_compile_exitcode(user_dict,exit_code)
                 set_compiler_version(user_dict,'-')
+
+                compile_c_command(exe_path,os.path.join(user_path,f))
+
+                set_compile_exitcode(user_dict,exit_code,user_dict)
                 set_run_mesurments('-1', '', user_dict)
-        
+
                 if not (int(exit_code) == 0 or int(exit_code) == 124) :
-                    print 'failed to run problem: ' + p_id + ' for: ' + user 
+                    print 'failed to run problem: ' + p_id + ' for: ' + user
                     print errors
+
+
 
 
 
@@ -60,13 +88,10 @@ def run_c(p_id, dict):
         filelist = [f for f in os.listdir(user_path) if '.' not in f]
         for f in filelist :
             print 'running c file for: ' + user + ' in problem ' + p_id
-
+            exe_file = f.split('.')[0]
+            exe_path = os.path.join(user_path,exe_file)
             # do run command
-            cmd = './' + os.path.join(user_path,f) + ' < ' + input_path
-            exit_code, errors = full_exe_cmd(cmd)
+            exit_code,errors = execute_c_command(exe_path, f, user_path, input_path, user_dict)
 
             # update dictonary with run mesurments
             set_run_mesurments(exit_code, errors, user_dict)
-
-
-
