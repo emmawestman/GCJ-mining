af4ce03a7dcc4d11977f051676c74fc940688d7f
Alexandra Back <alexandra1.back@gmail.com>
2017-05-08 13:10:42 +0200

    fixes

--- compile/compile_c/compile_c.py
+++ compile/compile_c/compile_c.py
@@ -24,7 +24,7 @@ def compile_c_command(exe_path, file_path, user_dict):
 
     if 'only allowed in ' in errors :
         cmd = 'gcc -std=c11 -o' + exe_path  + ' ' + file_path
-		if len (error_code_to_csv)>:0
+		if len (error_code_to_csv)>0:
 		    error_code_to_csv = error_code_to_csv + "&"
 		error_code_to_csv = error_code_to_csv + " lm "
         exit_code, errors = run_process(cmd)
