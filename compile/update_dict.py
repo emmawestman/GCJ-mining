d06da442c4bfaa529e221e2d15f2bb5c0be88305
Emma Westman <eewestman@gmail.com>
2017-06-02 17:53:13 +0200

    added none case in error msgs

--- compile/update_dict.py
+++ compile/update_dict.py
@@ -155,13 +155,11 @@ def set_compile_error_msg(user_dict, msg, exit_code, lang):
         msg = 'Command not found'
     else:
         msg = str(msg)
-        print type(msg)
+
         if 'None' == msg:
             msg = '-'
         else:
             msg = format_error_msg(msg, lang)
-    print msg
-    print type(msg)
     print 'Writing: ' + msg
     set_column_in_user_dict(user_dict,'compile_error_msg', msg)
 
