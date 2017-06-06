af38e8c0ca9525a0c30fb3b85fa3bbf343f9ef4e
alexandraback <alexandra1.back@gmail.com>
2017-06-06 11:54:00 +0200

    split files

--- compile/compilegcj.py
+++ compile/compilegcj.py
@@ -74,21 +74,22 @@ def compile_language(language, p_id, dict,user_ids):
 
 def compile_all():
     path = os.path.join(get_HOME_PATH(), 'GCJ-backup', 'rerun_timeout')
-    time_outfiles = os.listdir(path)
-    for file_time_out in time_outfiles:
-        with open(os.path.join(path,file_time_out), 'r') as f :
-            first_line = f.readline()
-            args = first_line.split()
-            l= args[0]
-            p_id =args[1]
-            filename = p_id+'.csv'
-            dict =read_csv_file(filename)
-            content = f.readlines()
-            user_ids = [x.strip() for x in content]
-            print 'Compiles and Runs: ' + l + ' in contest: ' + p_id
-            remove_old_files(l, p_id)
-            compile_language(l, p_id, dict,user_ids)
-            clean_home_dir()
-            write_to_csv_file(filename, dict)
+    time_outfolders = os.listdir(path)
+    for file_time_folder in time_outfolders:
+        for file_time_out in os.listdir(os.path.join(path,file_time_folder)):
+            with open(os.path.join(path,file_time_folder,file_time_out), 'r') as f :
+                first_line = f.readline()
+                args = first_line.split()
+                l= args[0]
+                p_id =args[1]
+                filename = p_id+'.csv'
+                dict =read_csv_file(filename)
+                content = f.readlines()
+                user_ids = [x.strip() for x in content]
+                print 'Compiles and Runs: ' + l + ' in contest: ' + p_id
+                remove_old_files(l, p_id)
+                compile_language(l, p_id, dict,user_ids)
+                clean_home_dir()
+                write_to_csv_file(filename, dict)
 
 compile_all()
